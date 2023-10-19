e7e45f59b6e92195b7bed0295cdbc72cb0264f78
Matthias <mpfeifer77@gmail.com>
2023-10-19 13:15:18 +0200

    Add files via upload
    Remove dependency to hydra. Remove custom aliases.

--- scratchy.el
+++ scratchy.el
@@ -1,11 +1,20 @@
-;; Replace scratch buffer by mode-specific scratch buffers
+;;; package --- scratch buffers with different major modes
+;;;
 
-(when (get-buffer "*scratch*")
-  (kill-buffer "*scratch*"))
+;;; Commentary:
+;;; scratch buffers with different major modes
+;;;
+;;;
+;;;
 
-(defvar scratchy-map (make-hash-table))
+;;; Code:
+
+(defvar scratchy-map (make-hash-table)
+  "This maps modes to buffers.  One buffer for each mode.")
 
 (defun scratchy-update-map (map)
+  "Expect MAP to be scratchy mapping from mode to buffer.
+Return list with existing buffers only."
   (let ((updated-list (make-hash-table)))
     (progn
       (dolist (mode (map-keys map))
@@ -15,57 +24,29 @@
       updated-list)))
 
 (defun scratchy-goto-mode (mode)
+  "Get the scratchy buffer for MODE.
+Display it in a newly created window in a newly created frame."
   (interactive)
   (let ((scratchy-buffer (gethash mode scratchy-map)))
     (when (or
            (not (buffer-live-p scratchy-buffer))
            (null scratchy-buffer))
       (progn
-        (setq scratchy-buffer (generate-new-buffer (format "*scratchy-%s*" (symbol-to-string mode))))
+        (setq scratchy-buffer (generate-new-buffer (format "*scratchy-%s*" (symbol-name mode))))
         (with-current-buffer scratchy-buffer
           (funcall mode))
         (puthash mode scratchy-buffer scratchy-map)))
     (select-frame (make-frame))
     (switch-to-buffer scratchy-buffer)))
 
-(defun scratchy-goto-text-mode ()
+(defconst scratchy-modes-list '("text-mode" "python-mode" "emacs-lisp-mode" "java-mode" "org-mode" "sh-mode" "json-mode" "sql-mode"))
+
+(defun scratchy-open-buffer ()
+  "Open a scratch buffer for a specific mode."
   (interactive)
-  (scratchy-goto-mode 'text-mode))
+  (let ((mode-of-choice (completing-read "Scratch-buffer for Which mode? " scratchy-modes-list)))
+    (scratchy-goto-mode (intern mode-of-choice))))
 
-(defun scratchy-goto-emacs-lisp-mode ()
-  (interactive)
-  (scratchy-goto-mode 'emacs-lisp-mode))
-
-(defun scratchy-goto-python-mode ()
-  (interactive)
-  (scratchy-goto-mode 'python-mode))
-
-(defun scratchy-goto-java-mode ()
-  (interactive)
-  (scratchy-goto-mode 'java-mode))
-
-(defun scratchy-goto-org-mode ()
-  (interactive)
-  (scratchy-goto-mode 'org-mode))
-
-(defun scratchy-goto-shl-mode ()
-  (interactive)
-  (scratchy-goto-mode 'sh-mode))
-
-(defhydra hydra-global-scratchy (:color blue :hint nil)
-  "
-Mode^^
-----------------------------------------------
-_t_ext-mode    _p_ython-mode        _o_rg-mode
-_j_ava-mode    emacs-_l_isp-mode    _s_h-mode
-"
-  ("t" scratchy-goto-text-mode)
-  ("l" scratchy-goto-emacs-lisp-mode)
-  ("p" scratchy-goto-python-mode)
-  ("j" scratchy-goto-java-mode)
-  ("o" scratchy-goto-org-mode)
-  ("s" scratchy-goto-shl-mode))
-
-(global-set-key (kbd "C-c s") 'hydra-global-scratchy/body)
+(global-set-key (kbd "C-c s") 'scratchy-open-buffer)
 
 (provide 'scratchy)
